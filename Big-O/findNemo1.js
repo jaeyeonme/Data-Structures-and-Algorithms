0c516781e040686cc6b33928464dc0f9d9d1a09a
JaeYeon33 <cjyeon1022@gmail.com>
2021-04-02 22:53:46 +0900

    2021-04-02 Big-O JS files practice

---
+++ Big-O/findNemo1.js
@@ -0,0 +1,16 @@
+const nemo = ['nemo'];
+const {performance} = require('perf_hooks');
+
+
+function findNemo1(array) {
+	let t0 = performance.now();
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] == 'nemo') {
+			console.log('Found NEMO!');
+		}
+	}
+	let t1 = performance.now()
+	console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds')
+}
+
+findNemo1(nemo);
