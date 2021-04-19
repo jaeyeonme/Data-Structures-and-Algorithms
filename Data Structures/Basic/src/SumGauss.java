306987637288a9df2e4e20fde62c7937f9106e4b
JaeYeon33 <cjyeon1022@gmail.com>
2021-04-20 02:48:20 +0900

    2021-04-20 Data Structure with Java study file

---
+++ Data Structures/Basic/src/SumGauss.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+// 가우스 공식에 의해 1, 2, ..., n의 합을 구한다.
+class SumGauss {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n의 값: ");
+        int n = stdIn.nextInt();
+
+        int sum = (n + 1) * (n / 2) + (n % 2 == 1 ? (n + 1) / 2 : 0); // 합
+
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+    }
+}
