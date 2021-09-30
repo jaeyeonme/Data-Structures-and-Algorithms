cd29b8af3303c42edc8d2ddb633165579ce716f4
JaeYeon33 <cjyeon1022@gmail.com>
2021-09-30 23:14:36 +0900

    2021-09-30 Algorithm study

---
+++ inflearn_java/src/string/StringRollOver.java
@@ -0,0 +1,51 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+ */
+class StringRollOver {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public ArrayList<String> solution2(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        StringRollOver T = new StringRollOver();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+}
